98032f5c939386251cc06a492ef2f5e472e2e35d
Roberto Frenna <roberto.frenna@gmail.com>
2018-11-20 10:50:40 +0100

    first commit using electron-vue

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+  import Home from '@/components/Main'
+
+  export default {
+    name: 'so-project',
+    components: {
+      Home
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>

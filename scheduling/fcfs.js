2abd2ce9e3cbdc655485c37e1d495378998873b2
Roberto Frenna <roberto.frenna@gmail.com>
2019-01-14 17:52:51 +0100

    scheduling/fcfs: migrate to new & quicker fast-forwarding algorithm

--- scheduling/fcfs.js
+++ scheduling/fcfs.js
@@ -1,31 +1,23 @@
 import { sortByArrivalOrIndex } from '../scheduling'
 
-
 export default function fcfs (processes) {
   // Sort the processes by their arrival time or their index if the arrival time is equal.
   let processQueue = sortByArrivalOrIndex (processes)
   let simulationResults = []
-  let currentlyExecuting
   let currentTime = 0
-  for (;;) {
+  while (processQueue.length) {
     // Find a process to schedule if we aren't already executing one.
-    if (!currentlyExecuting) {
-      // The simulation is done if there are no more processes.
-      if (!processQueue.length)
-        break
-      // If the next process in the queue has arrived, dequeue it and start the execution.
-      if (currentTime >= processQueue[0].arrival) {
-        currentlyExecuting = processQueue.shift()
-        currentlyExecuting.startTime = currentTime
-      }
-    } else if (currentTime >= currentlyExecuting.startTime + currentlyExecuting.duration) {
-      // The current process is over.
-      currentlyExecuting.endTime = currentTime
+    if (currentTime >= processQueue[0].arrival) {
+      // Dequeue the topmost process.
+      let pendingProcess = processQueue.shift()
+      pendingProcess.startTime = currentTime
+      // Fast forward to its end.
+      currentTime += pendingProcess.duration
+      pendingProcess.endTime = currentTime
+      simulationResults[pendingProcess.index] = pendingProcess
       console.log('Execution of %s (startTime = %d, duration = %d, endtime = %d) finished.',
-        currentlyExecuting.name, currentlyExecuting.startTime,
-        currentlyExecuting.duration, currentTime)
-      simulationResults[currentlyExecuting.index] = currentlyExecuting
-      currentlyExecuting = null
+        pendingProcess.name, pendingProcess.startTime,
+        pendingProcess.duration, currentTime)
       continue // Continue without advancing the time.
     }
     ++currentTime

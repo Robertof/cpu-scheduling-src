6f196c70ecee175240e05340a67135d7d4b8456b
Roberto Frenna <roberto.frenna@gmail.com>
2019-01-22 18:22:32 +0100

    project: add v-tooltip

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 //import BootstrapVue from 'bootstrap-vue'
 import App from './App'
+import VTooltip from 'v-tooltip'
 import 'bootstrap/dist/css/bootstrap-grid.css'
 //import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -8,6 +9,7 @@ if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
 //Vue.use(BootstrapVue)
+Vue.use (VTooltip)
 
 /* eslint-disable no-new */
 new Vue({
